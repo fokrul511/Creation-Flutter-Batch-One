1b1667946930ae99d089a08fd23363389abc20e6
fokrul511 <islamfokrul2002@gmail.com>
2024-07-12 12:02:00 +0600

    module 2 fist class

--- creation_flutter_batch_01/basic dart/for_loop.dart
+++ creation_flutter_batch_01/basic dart/for_loop.dart
@@ -1,16 +1,7 @@
 void main(){
-  int x = 20;
-  int y = 30;
-  var add = x + y;
-  var sub = x - y;
-  var mul = x * y;
-  var div = x / y;
-  var mod = x % y;
-  print(add);
-  print(sub);
-  print(mul);
-  print(div);
-  print(mod);
+ for (var i = 0; i<100; i++){
+   print(i);
+ }
 
 
 }
